5488030cf416b5248420eb18932b1b54584f3496
aleksvvv <adm-shop-001@yandex.ru>
2023-03-18 19:18:18 +0300

    Data layer.

--- app/src/main/java/com/bignerdranch/android/composition/domain/entity/Question.kt
+++ app/src/main/java/com/bignerdranch/android/composition/domain/entity/Question.kt
@@ -1,7 +1,7 @@
 package com.bignerdranch.android.composition.domain.entity
 
 data class Question(
-    val sum:Int,
-    val visibleNumber: String,
+    val sum: Int,
+    val visibleNumber: Int,
     val options: List<Int>
 )
