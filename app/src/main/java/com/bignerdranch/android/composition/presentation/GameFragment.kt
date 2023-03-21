0749b13f853860a6350dc6a2c5a2e063efc2c5c9
aleksvvv <adm-shop-001@yandex.ru>
2023-03-21 15:06:28 +0300

    GameViewModel

--- app/src/main/java/com/bignerdranch/android/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/bignerdranch/android/composition/presentation/GameFragment.kt
@@ -1,10 +1,14 @@
 package com.bignerdranch.android.composition.presentation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.bignerdranch.android.composition.R
 import com.bignerdranch.android.composition.databinding.FragmentGameBinding
 import com.bignerdranch.android.composition.domain.entity.GameResult
@@ -12,53 +16,91 @@ import com.bignerdranch.android.composition.domain.entity.GameSettings
 import com.bignerdranch.android.composition.domain.entity.Level
 
 
-class GameFragment:Fragment() {
+class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
     private lateinit var level: Level
     private var _binding: FragmentGameBinding? = null
-    private val binding:FragmentGameBinding
-    get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+    private val binding: FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
+    private lateinit var tvSum: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         parsArgs()
+
+
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentGameBinding.inflate(inflater,container,false)
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tvSum.setOnClickListener {
-            launchGameFinishedFragment(
-                GameResult(false,4,44,
-                    GameSettings(44,4,44,4)))
-        }
+
+        viewModel = ViewModelProvider(this)[GameViewModel::class.java]
+        initView()
+        viewModel.startGame(level)
+        observeViewModel()
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-    private fun parsArgs(){
+
+    private fun parsArgs() {
         requireArguments().getParcelable<Level>(LEVEL_GAME)?.let {
             level = it
         }
     }
-    private fun launchGameFinishedFragment(gameResult: GameResult){
+
+    private fun observeViewModel() {
+        viewModel.question.observe(viewLifecycleOwner) {
+            binding.tvSum.text = it.sum.toString()
+            binding.tvLeftNumber.text = it.visibleNumber.toString()
+            val options = it.options
+            val index = 0
+            for (i in options){
+
+            }
+            binding.tvOption1.text = options[0].toString()
+            binding.tvOption2.text = options[1].toString()
+            binding.tvOption3.text = options[2].toString()
+            binding.tvOption4.text = options[3].toString()
+            binding.tvOption5.text = options[4].toString()
+            binding.tvOption6.text = options[5].toString()
+        }
+        viewModel.formattedTime.observe(viewLifecycleOwner){
+            binding.tvTimer.text = it
+        }
+        viewModel.gameResult.observe(viewLifecycleOwner){
+                      launchGameFinishedFragment( it)
+        }
+    }
+
+    private fun initView() {
+
+    }
+
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
         requireActivity().supportFragmentManager
             .beginTransaction()
-            .replace(R.id.main_container,GameFinishedFragment.newInstance(gameResult))
+            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
             .addToBackStack(null)
             .commit()
     }
-    companion object{
+
+    companion object {
         private const val LEVEL_GAME = "level"
-        fun newInstance(level: Level): GameFragment{
+        fun newInstance(level: Level): GameFragment {
 
             return GameFragment().apply {
                 arguments = Bundle().apply {
---
+++ app/src/main/java/com/bignerdranch/android/composition/presentation/GameViewModel.kt
@@ -0,0 +1,140 @@
+package com.bignerdranch.android.composition.presentation
+
+import android.app.Application
+import android.os.CountDownTimer
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.composition.R
+import com.bignerdranch.android.composition.data.GameRepositoryImpl
+import com.bignerdranch.android.composition.domain.entity.GameResult
+import com.bignerdranch.android.composition.domain.entity.GameSettings
+import com.bignerdranch.android.composition.domain.entity.Level
+import com.bignerdranch.android.composition.domain.entity.Question
+import com.bignerdranch.android.composition.domain.usecases.GenerateQuestionsUseCase
+import com.bignerdranch.android.composition.domain.usecases.GetGameSettingsUseCase
+import kotlin.concurrent.timer
+
+class GameViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository = GameRepositoryImpl
+    private val getGameSettingsUseCase = GetGameSettingsUseCase(repository)
+    private val generateQuestionsUseCase = GenerateQuestionsUseCase(repository)
+    private lateinit var level: Level
+    private lateinit var gameSettings: GameSettings
+    private var timer:CountDownTimer? = null
+    private val context = application
+
+
+    private val _question = MutableLiveData<Question>()
+    val question: LiveData<Question>
+        get() = _question
+    private val _formattedTime = MutableLiveData<String>()
+    val formattedTime:LiveData<String>
+    get() = _formattedTime
+    private val _percentOfRightAnswers = MutableLiveData<Int>()
+    val percentOfRightAnswers: LiveData<Int>
+        get() = _percentOfRightAnswers
+
+    private val _progressAnswers = MutableLiveData<String>()
+    val progressAnswers: LiveData<String>
+        get() = _progressAnswers
+
+    private val _enoughCount = MutableLiveData<Boolean>()
+    val enoughCount: LiveData<Boolean>
+        get() = _enoughCount
+
+    private val _enoughPercent = MutableLiveData<Boolean>()
+    val enoughPercent: LiveData<Boolean>
+        get() = _enoughPercent
+
+    private val _gameResult = MutableLiveData<GameResult>()
+    val gameResult: LiveData<GameResult>
+        get() = _gameResult
+
+    private var countOfRightAnswers = 0
+    private var countOfQuestions = 0
+
+    fun startGame(level: Level) {
+       getGameSettings(level)
+        startTimer()
+        generateQuestion()
+        updateProgress()
+    }
+    fun chooseAnswer(number: Int) {
+        checkAnswer(number)
+        updateProgress()
+        generateQuestion()
+    }
+    private fun updateProgress() {
+        val percent = calculatePercentOfRightAnswers()
+        _percentOfRightAnswers.value = percent
+        _progressAnswers.value = String.format(
+            context.resources.getString(R.string.progress_answers),
+            countOfRightAnswers,
+            gameSettings.minCountOfRightAnswers
+        )
+        _enoughCount.value = countOfRightAnswers >= gameSettings.minCountOfRightAnswers
+        _enoughPercent.value = percent >= gameSettings.minPercentOfRightAnswers
+    }
+
+    private fun calculatePercentOfRightAnswers(): Int {
+        if (countOfQuestions == 0) {
+            return 0
+        }
+        return ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+    }
+    private fun checkAnswer(number: Int) {
+        val rightAnswer = question.value?.rightAnswer
+        if (number == rightAnswer) {
+            countOfRightAnswers++
+        }
+        countOfQuestions++
+    }
+
+    private fun startTimer(){
+        val timer = object : CountDownTimer(
+            gameSettings.gameTimeInSeconds* MILLIS_IN_SECONDS,
+            MILLIS_IN_SECONDS){
+            override fun onTick(millisUntilFinished: Long) {
+                _formattedTime.value = timeFormatted(millisUntilFinished)
+            }
+            override fun onFinish() {
+                finishGame()
+            }
+        }
+            timer.start()
+    }
+    private fun timeFormatted(millis: Long):String{
+        val second = millis / MILLIS_IN_SECONDS
+        val minut = second / SECONDS
+        val secondLast = second - (minut * SECONDS)
+        return String.format("%02d : %02d",minut,secondLast)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer?.cancel()
+    }
+    private fun finishGame(){
+       _gameResult.value = GameResult(
+            winner = enoughPercent.value == true && enoughCount.value == true,
+            countOfRightAnswers = countOfRightAnswers,
+            countOfQuestions = countOfQuestions,
+            gameSettings = gameSettings
+        )
+
+    }
+    private fun getGameSettings(level: Level) {
+        this.level = level
+        this.gameSettings = getGameSettingsUseCase(level)
+    }
+    private fun generateQuestion(){
+        val maxSumValue = gameSettings.maxSumValue
+        _question.value = generateQuestionsUseCase(maxSumValue)
+    }
+    companion object{
+        const val MILLIS_IN_SECONDS = 1000L
+        const val SECONDS = 60
+    }
+}
